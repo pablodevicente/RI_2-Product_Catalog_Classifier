df6b6326d3ebf2c1a24a4e052427b2f29114c907
yenyyo <pdevicente@hotmail.com>
2025-01-29 16:33:32 +0100

    Stop tracking aux_files

--- 01-code/test.py
+++
@@ -1,7 +0,0 @@
-import text_preprocessing as txtp
-
-# Preprocess text using default preprocess functions in the pipeline
-text_to_process = 'Helllo, I am John Doe!!! My email is john.doe@email.com. Visit our website www.johndoe.com'
-preprocessed_text = txtp.preprocess_text(text_to_process)
-print(preprocessed_text)
-# output: hello email visit website
