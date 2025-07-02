b29285bad90315cbee6b6ac52fdfe58eb327a81b
yenyyo <pdevicente@hotmail.com>
2025-07-02 18:56:22 +0200

    updated download_pdfs

---
+++ 01-code/download_pdfs/config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# ─────────────────────────────
+# PDF Download Configuration
+# ─────────────────────────────
+BASE_PATH = "../02-data/01-pdfs/"
+URL_PATH = "../02-data/digikeys_urls.txt"
